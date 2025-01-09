520ab337a29e9f2498363a2c1edeb318db1d7c2d
Fateeh89 <fatihtop123@gmail.com>
2025-01-09 18:49:58 +0700

    Update logging.py

--- utils/logging.py
+++ utils/logging.py
@@ -1,11 +1,41 @@
-import logging
+# utils/logging.py
 
-def setup_logging():
-    logging.basicConfig(
-        level=logging.INFO,
-        format='%(asctime)s - %(levelname)s - %(message)s',
-        handlers=[
-            logging.FileHandler("app.log"),
-            logging.StreamHandler()
-        ]
-    )
+import logging
+from logging.handlers import RotatingFileHandler
+from flask import current_app
+
+class Logger:
+    def __init__(self, app):
+        self.app = app
+        self.logger = logging.getLogger(__name__)
+        self.logger.setLevel(logging.DEBUG)
+
+        formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+
+        file_handler = RotatingFileHandler('web_automation_tool.log', maxBytes=1000000, backupCount=1)
+        file_handler.setFormatter(formatter)
+
+        stream_handler = logging.StreamHandler()
+        stream_handler.setFormatter(formatter)
+
+        self.logger.addHandler(file_handler)
+        self.logger.addHandler(stream_handler)
+
+    def debug(self, message):
+        self.logger.debug(message)
+
+    def info(self, message):
+        self.logger.info(message)
+
+    def warning(self, message):
+        self.logger.warning(message)
+
+    def error(self, message):
+        self.logger.error(message)
+
+    def critical(self, message):
+        self.logger.critical(message)
+
+def init_app(app):
+    logger = Logger(app)
+    return logger
